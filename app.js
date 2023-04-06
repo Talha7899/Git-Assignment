2427464a6b051bc46c66c90b48ace44cea276767
Talha Shehzore Khan <shehzoret89@gmail.com>
2023-04-06 13:34:09 -0700

    Add onclick functionality in Js file for addition subtraction multiplication division

---
+++ app.js
@@ -0,0 +1,20 @@
+function Add(){
+let num1=document.getElementById('First').value;
+let num2=document.getElementById('Second').value;
+document.getElementById('Result').innerHTML=num1+num2;
+};
+function Subtract(){
+let num1=document.getElementById('First').value;
+let num2=document.getElementById('Second').value;
+document.getElementById('Result').innerHTML=num1-num2;
+};
+function Product(){
+let num1=document.getElementById('First').value;
+let num2=document.getElementById('Second').value;
+document.getElementById('Result').innerHTML=num1*num2;
+};
+function Division(){
+let num1=document.getElementById('First').value;
+let num2=document.getElementById('Second').value;
+document.getElementById('Result').innerHTML=num1/num2;
+};
