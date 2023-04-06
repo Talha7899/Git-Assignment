5e6e48955b9db7d5991b8d9dbbc2d7f1f3691b11
Talha Shehzore Khan <shehzoret89@gmail.com>
2023-04-06 13:48:50 -0700

    Modification in html file is Add h3 tag and modification in js file is Add Parseint before Addition Operation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function Add(){
 let num1=document.getElementById('First').value;
 let num2=document.getElementById('Second').value;
-document.getElementById('Result').innerHTML=num1+num2;
+document.getElementById('Result').innerHTML=parseInt(num1)+parseInt(num2);
 };
 function Subtract(){
 let num1=document.getElementById('First').value;
